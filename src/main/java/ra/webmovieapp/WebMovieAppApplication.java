845e40b51fd7c0e87ee5eeaf24e06650e343e62c
nhatthan2k <133490159+nhatthan2k@users.noreply.github.com>
2024-04-01 16:27:08 +0700

    first commit

---
+++ src/main/java/ra/webmovieapp/WebMovieAppApplication.java
@@ -0,0 +1,13 @@
+package ra.webmovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMovieAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ra/webmovieapp/WebMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.webmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
