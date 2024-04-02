6986cc6c5aeb08cfe1bff116674334ca95509389
quocdat19 <quocdat19092000@gmail.com>
2024-04-02 11:01:47 +0700

    add_database

---
+++ src/main/java/ra/webmovieapp/model/entity/GenreDetail.java
@@ -0,0 +1,25 @@
+package ra.webmovieapp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table(name = "genre_detail")
+@IdClass(GenreDetailId.class)
+public class GenreDetail {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private Genre genre ;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "movie_id", referencedColumnName = "id")
+    private Movie movie;
+
+
+}
---
+++ src/main/java/ra/webmovieapp/model/entity/User.java
@@ -0,0 +1,42 @@
+package ra.webmovieapp.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import ra.webmovieapp.model.base.BaseModel;
+
+import java.util.List;
+import java.util.Set;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class User extends BaseModel {
+    private String username;
+    private String email;
+    private String fullName;
+    private Boolean status;
+    private String phone;
+    private String address;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Follow> follows;
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Evaluate> evaluates;
+
+
+
+
+
+
+}
