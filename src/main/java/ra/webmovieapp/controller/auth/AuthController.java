cac3fbf83d497c04d7bf51a2c51444bbcbf36e65
Pham Van Tung <Phamtungkp2002@gmail.com>
2024-04-11 15:13:21 +0700

    bigBug

--- src/main/java/ra/webmovieapp/controller/auth/AuthController.java
+++ src/main/java/ra/webmovieapp/controller/auth/AuthController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ra.webmovieapp.exception.CustomException;
 import ra.webmovieapp.model.dto.auth.LoginRequest;
 import ra.webmovieapp.model.dto.auth.RegisterRequest;
@@ -19,6 +16,7 @@ import ra.webmovieapp.service.UserService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
+@CrossOrigin("*")
 public class AuthController {
     @Autowired
     private UserService userService;
